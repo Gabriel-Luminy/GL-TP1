230b136e2673ee5ba53fb15164240f4ae93ce6ad
Gabriel-Luminy <gabriel@ladet.net>
2015-10-10 16:08:34 +0200

    JUnit Tests5

--- test/src/MainTest.java
+++ test/src/MainTest.java
@@ -1,15 +1,22 @@
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(JUnit4.class)
 
+public class MainTest extends TestCase {
 
-public class MainTest {
-
+	
 	@Test
 	public void test() throws Exception {
 		assertEquals(5, Main.additionner(2, 3));
-		assertEquals(10, Main.additionner(10, 0));
-			
+		assertEquals(7, Main.additionner(4, 3));
+		
 	}
+	
+
 
 }
